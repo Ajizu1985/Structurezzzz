71673c13077b14bacd9fc79b26e85194ff9006e9
Ajizu1985 <az3406.aa@gmail.com>
2021-11-25 20:56:19 +0500

    New

--- 3.c
+++ 3.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -10,33 +11,37 @@ struct Publish {
 
 int main() {
     
-    int n=4;
-    struct Publish book[n];
+    struct Publish book[4];
+    int z;
     
     strcpy(book[0].name, "ACCA");
     strcpy(book[0].author, "BPP");
-    book[0].year = 2021;
+    book[0].year=2021;
 
     strcpy(book[1].name, "Development economics");
     strcpy(book[1].author, "Todaro M., Smith S.");
-    book[1].year = 2010;
+    book[1].year=2020;
 
     strcpy(book[2].name, "Python: Basics");
     strcpy(book[2].author, "Chen D.");
-    book[2].year = 2012;
+    book[2].year=2012;
 
     strcpy(book[3].name, "Macroeconomics");
     strcpy(book[3].author, "Bernanke B.");
-    book[3].year = 2012;
+    book[3].year=2015;
     
     printf("Year of publication: ");
-    for(int i=0; i<n; i++){
-    if (book[i].year == 2012){
-    scanf("%d", &book[i].year);
-    printf("Book name: %s\n", book[i].name);
-    printf("Book author: %s\n", book[i].author);
-    printf("Publication date: %d\n", book[i].year);
+    scanf("%d", &z);
+    
+    for(int i=0; i<4; i++){
+    if(book[i].year==z){
+   // printf("%d", book[i].year);
+    {
+   printf("Book name: %s\n", book[i].name);
+   printf("Book author: %s\n", book[i].author);
+   printf("Publication date: %d\n", book[i].year);
   }
+    }
     }
   return 0;
 }
